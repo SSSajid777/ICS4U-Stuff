a1f0911d0e3c1cb19b1bf634dad941a8cd675318
Kota-Kun26 <sajidihsanabdullah@gmail.com>
2026-02-18 13:51:51 -0500

    e

---
+++ Programming (Sajid, ICS4U)/commenting.java
@@ -0,0 +1,37 @@
+//This is a header with single lines
+//Name: Sajid
+//Date: Feb 12, 2026
+//Description: This is a sample header 
+
+/*
+This is a header with multi lines
+Name: Sajid
+Date: Feb 12, 2026
+Description: this is a sample header
+ */
+public class commenting {
+
+    public static void main(String[] args) {
+        //int l=3;
+        //int w=4;
+        System.out.println("""
+ .--.                            .--.              
+: .; :                          : .-'              
+:    :.--.  .--.  .--.     .--. : `;    .--.       
+: :: :: ..'' '_.'' .; ;   ' .; :: :    ' .; ;      
+:_;:_;:_;  `.__.'`.__,_;  `.__.':_;    `.__,_;     
+                                                   
+                                                   
+.---.             .-.                   .-.        
+: .; :           .' `.                  : :        
+:   .' .--.  .--.`. .'.--.  ,-.,-. .--. : :   .--. 
+: :.`.' '_.''  ..': :' .; ; : ,. :' .; :: :_ ' '_.'
+:_;:_;`.__.'`.__.':_;`.__,_;:_;:_;`._. ;`.__;`.__.'
+                                   .-. :           
+                                   `._.'           
+        """);
+        int l=3, w=4;
+        System.out.println("The area of the rectangle with length " + l + " and width " + w + " is " + l*w);
+
+    }
+}
