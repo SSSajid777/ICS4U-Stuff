0e410662f44b62c70666bcccf432a6f9cf3b23cf
Kota-Kun26 <sajidihsanabdullah@gmail.com>
2026-02-18 13:54:24 -0500

    Update commenting.java

--- Programming (Sajid, ICS4U)/commenting.java
+++ Programming (Sajid, ICS4U)/commenting.java
@@ -12,6 +12,21 @@ Description: this is a sample header
 public class commenting {
 
     public static void main(String[] args) {
+
+        // Source - https://stackoverflow.com/a/5762502
+// Posted by WhiteFang34, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-18, License - CC BY-SA 3.0
+
+String ANSI_RESET = "\u001B[0m";
+String ANSI_BLACK = "\u001B[30m";
+String ANSI_RED = "\u001B[31m";
+String ANSI_GREEN = "\u001B[32m";
+String ANSI_YELLOW = "\u001B[33m";
+String ANSI_BLUE = "\u001B[34m";
+String ANSI_PURPLE = "\u001B[35m";
+String ANSI_CYAN = "\u001B[36m";
+String ANSI_WHITE = "\u001B[37m";
+
         //int l=3;
         //int w=4;
         System.out.println("""
