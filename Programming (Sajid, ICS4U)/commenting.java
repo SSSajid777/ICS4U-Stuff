2a564429ca6a66717a12ac801e7d45bb2a63357d
Kota-Kun26 <sajidihsanabdullah@gmail.com>
2026-02-18 14:02:56 -0500

    Update commenting.java

--- Programming (Sajid, ICS4U)/commenting.java
+++ Programming (Sajid, ICS4U)/commenting.java
@@ -17,19 +17,19 @@ public class commenting {
 // Posted by WhiteFang34, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-02-18, License - CC BY-SA 3.0
 
-String ANSI_RESET = "\u001B[0m";
-String ANSI_BLACK = "\u001B[30m";
-String ANSI_RED = "\u001B[31m";
-String ANSI_GREEN = "\u001B[32m";
-String ANSI_YELLOW = "\u001B[33m";
-String ANSI_BLUE = "\u001B[34m";
-String ANSI_PURPLE = "\u001B[35m";
-String ANSI_CYAN = "\u001B[36m";
-String ANSI_WHITE = "\u001B[37m";
+String RESET = "\u001B[0m";
+String BLACK = "\u001B[30m";
+String RED = "\u001B[31m";
+String GREEN = "\u001B[32m";
+String YELLOW = "\u001B[33m";
+String BLUE = "\u001B[34m";
+String PURPLE = "\u001B[35m";
+String CYAN = "\u001B[36m";
+String WHITE = "\u001B[37m";
 
         //int l=3;
         //int w=4;
-    System.out.println(ANSI_CYAN + """
+    System.out.println(CYAN + """
  .--.                            .--.              
 : .; :                          : .-'              
 :    :.--.  .--.  .--.     .--. : `;    .--.       
@@ -44,7 +44,7 @@ String ANSI_WHITE = "\u001B[37m";
 :_;:_;`.__.'`.__.':_;`.__,_;:_;:_;`._. ;`.__;`.__.'
                                    .-. :           
                                    `._.'           
-        """);
+        """+ RESET);
         int l=3, w=4;
         System.out.println("The area of the rectangle with length " + l + " and width " + w + " is " + l*w);
 
