5b010326086234700eb57991747ae98a769c073a
Kota-Kun26 <sajidihsanabdullah@gmail.com>
2026-02-20 13:42:04 -0500

    Update Area_Circle.java

--- Programming (Sajid, ICS4U)/Area_Circle.java
+++ Programming (Sajid, ICS4U)/Area_Circle.java
@@ -1,10 +1,36 @@
+/*
+Name: Sajid Abdullah
+Date: Feb 20, 2026
+Description:
+The program first asks the user for their name and greets them with a welcome message.
+then it will ask them for a radius and calculate the area/circumference of a circle.
+Resources:
+https://www.asciiart.eu/text-to-ascii-art#google_vignette (ASCII art for titles)
+https://emojicombos.com/falling-boulder-ascii-art (ASCII art for images)
+*/
+
+
+
 import java.util.Scanner;
 public class Area_Circle {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("What is your name?");
+
+        String RESET = "\u001B[0m"; // Resets the colour back to default
+        String BLACK = "\u001B[30m";
+        String RED = "\u001B[31m";
+        String GREEN = "\u001B[32m";
+        String YELLOW = "\u001B[33m";
+        String BLUE = "\u001B[34m";
+        String PURPLE = "\u001B[35m";
+        String CYAN = "\u001B[36m";
+        String WHITE = "\u001B[37m";
+        // prints ASCII art title
+
+        System.out.println(RED + "What is your name?"+ RESET);
         String user_name = scan.next();
+        System.out.println("Welcome to the Area and Perimeter of a Circle Program, " + user_name);
 
         double pi=3.14;
 
@@ -12,10 +38,10 @@ public class Area_Circle {
         int user_radius = scan.nextInt();
 
         double Area = (pi*(user_radius*user_radius));
-        System.out.println("The Area is " + Area);
+        System.out.println("The Area is " + (int)Area);
 
         double Perimenter = (2*pi*user_radius);
-        System.out.println("The Perimeter is " + Perimenter);
+        System.out.println("The Perimeter is " + (int)Perimenter);
 
 
 
