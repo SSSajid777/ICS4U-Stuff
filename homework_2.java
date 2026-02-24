9396accdfa5846a80cc649883455da937cc6b5c6
Kota-Kun26 <sajidihsanabdullah@gmail.com>
2026-02-24 14:23:40 -0500

    Create homework_2.java

---
+++ homework_2.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class homework_2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String RESET = "\u001B[0m"; // Resets the colour back to default
+        String BLACK = "\u001B[30m";
+        String RED = "\u001B[31m";
+        String GREEN = "\u001B[32m";
+        String YELLOW = "\u001B[33m";
+        String BLUE = "\u001B[34m";
+        String PURPLE = "\u001B[35m";
+        String CYAN = "\u001B[36m";
+        String WHITE = "\u001B[37m";
+        // prints ASCII art title
+
+        System.out.println(YELLOW + """
+ .--.                .-.                                
+: .--'               : :                                
+: : _ .--.  .--.   .-' : .--.                           
+: :; :: ..'' .; ; ' .; :' '_.'                          
+`.__.':_;  `.__,_;`.__.'`.__.'                          
+                                                        
+                                                        
+ .--.        .-.              .-.         .-.           
+: .--'       : :              : :        .' `.          
+: :    .--.  : :   .--. .-..-.: :   .--. `. .'.--. .--. 
+: :__ ' .; ; : :_ '  ..': :; :: :_ ' .; ; : :' .; :: ..'
+`.__.'`.__,_;`.__;`.__.'`.__.'`.__;`.__,_;:_;`.__.':_;                          
+           
+                """ + RESET);
+
+        System.out.println(RED + "What is your name?"+ RESET + GREEN);
+        String user_name = scan.next();
+        System.out.println(RESET + BLUE + "Welcome to the Grade Calculator Program, " + user_name + RESET);
+        
+        System.out.println("Please enter your test percentage (without percentage sign)");
+        int grade = scan.nextInt();
+
+        if (grade >= 90 && grade <= 100) {
+            System.out.println("Level 4+");
+
+        }
+        else if (grade >= 80 && grade <= 89) {
+            System.out.println("Level 4");
+
+        }
+        else if (grade >= 70 && grade < 79) {
+            System.out.println("Level 3");
+
+        }
+        else if (grade >= 60 && grade < 69) {
+            System.out.println("Level 2");
+
+        }
+        else if (grade < 60) {
+            System.out.println("Keep practicing!");
+
+        }
+        else if (grade < 0 || grade > 100) {
+            System.out.println("Invalid Grade");
+        
+        }
+        
+
+    }
+}
