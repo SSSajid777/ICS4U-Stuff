2c18396d49120469c8b803ef41056fadf3a010c1
Kota-Kun26 <sajidihsanabdullah@gmail.com>
2026-02-24 13:39:12 -0500

    Update test4.java

--- test4.java
+++ test4.java
@@ -1,6 +1,11 @@
+
+import java.util.Scanner;
+
 public class test4 {
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
 
         //we are looking at functions involving strings
          String name1 = "ApplePie";
@@ -9,8 +14,14 @@ public class test4 {
          System.out.println(name1.toUpperCase()); //returns the string to all uppercase
          System.out.println(name1.toLowerCase()); //returns the string to all lowercase
 
+         //gets rid of the spaces before and after and makes the input lowercase
          String name2 = "    Hello    ".strip();
          System.out.println(name2);
+         String name3 = scan.nextLine().strip().toLowerCase();
+         System.out.println(name3);
+
+
+
          
          
 
