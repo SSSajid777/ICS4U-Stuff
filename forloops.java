d236bde62dfb9be73009fea29fd96a07713d8e57
SSSajid <sajidihsanabdullah@gmail.com>
2026-02-26 14:21:07 -0500

    Update forloops.java

--- forloops.java
+++ forloops.java
@@ -13,13 +13,24 @@ public class forloops{
             /*
         for(int i = 1; i<6; i++){
             System.out.print(i +" * 6 = " + (i*6) +"      ");
-            */
+            
         System.out.println("Enter the number you want the times table for.");
         int multiplier = scan.nextInt();
         for(int i = 1; i<6; i++){
             System.out.println(i +" * " + multiplier + " = " + (i*multiplier));
+            */
+        for (int i = 10; i > 0; i--){
+            System.out.println(i);
+            try{
+                Thread.sleep(1000);
+            }
+            catch(Exception e){
+                
+            }
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
  
 
         }
     }
-}
