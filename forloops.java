def7339e3e04e9fdd44608184a895d6f93d205f7
SSSajid <sajidihsanabdullah@gmail.com>
2026-02-26 13:41:01 -0500

    Update forloops.java

--- forloops.java
+++ forloops.java
@@ -4,7 +4,8 @@ public class forloops{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         //i=i+1 or i++ or i+=1
-        for(int i = 1; i<6; i =i+1);{
+        for(int i = 1; i<6; i++){
+            System.out.print(i+" ");
 
         }
     }
