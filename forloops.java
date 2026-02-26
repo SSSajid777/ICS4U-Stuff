92bee87810f10d36e584a9f6e537de3e90ddc846
SSSajid <sajidihsanabdullah@gmail.com>
2026-02-26 13:39:20 -0500

    Update forloops.java

--- forloops.java
+++ forloops.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class forloops{
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        //i=i+1 or i++ or i+=1
+        for(int i = 1; i<6; i =i+1);{
+
+        }
     }
 }
