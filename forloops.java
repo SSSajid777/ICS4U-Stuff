7ed2569344947d09681f8a5bcbeb81acfa086857
SSSajid <sajidihsanabdullah@gmail.com>
2026-02-26 14:05:02 -0500

    times table

--- forloops.java
+++ forloops.java
@@ -3,9 +3,22 @@ import java.util.Scanner;
 public class forloops{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        /* 
         //i=i+1 or i++ or i+=1
         for(int i = 1; i<6; i++){
-            System.out.print(i+" ");
+            System.out.println(i+" ");
+            */
+
+
+            /*
+        for(int i = 1; i<6; i++){
+            System.out.print(i +" * 6 = " + (i*6) +"      ");
+            */
+        System.out.println("Enter the number you want the times table for.");
+        int multiplier = scan.nextInt();
+        for(int i = 1; i<6; i++){
+            System.out.println(i +" * " + multiplier + " = " + (i*multiplier));
+ 
 
         }
     }
--- homework3.java
+++ homework3.java
@@ -1,5 +1,6 @@
-public class homework3 {
+import java.util.Scanner;
+public class homework3{
     public static void main(String[] args) {
-        
-    }  
+        Scanner scan = new Scanner(System.in);
+    }
 }
