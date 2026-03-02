39532d9e0489e0cc67151e10551b44d0d4eec344
SSSajid <sajidihsanabdullah@gmail.com>
2026-03-02 13:59:36 -0500

    Update homework3.java

--- homework3.java
+++ homework3.java
@@ -3,6 +3,14 @@ public class homework3{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        
+        System.out.println("Enter a positive integer"); //1. Asks the user to enter a positive integer n.
+        int number=scan.nextInt();
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            if(i%2==0){
+                sum=sum+i;
+            }
+        }
+        System.out.println("The sum of all even numbers from 1 to "+number+" is "+sum);
     }
 }
