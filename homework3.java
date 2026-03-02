4d42e2829fed1f2ace1a82e4a1b6706acb9fe5d4
SSSajid <sajidihsanabdullah@gmail.com>
2026-03-02 14:21:45 -0500

    Update homework3.java

--- homework3.java
+++ homework3.java
@@ -11,6 +11,31 @@ public class homework3{
                 sum+=i;
             }
         }
-        System.out.println("The sum of all even numbers from 1 to "+number+" is "+sum);
+        System.out.println("The sum of all even numbers from 1 to "+ number +" is " + sum);
+
+
+        System.out.println("Enter a word to count how many vowels in a word");
+        int sum_vowel= 0;
+        String word=scan.next();
+        for(int i=0; i<word.length(); i++){ //how do i make it work for both uppercase and lowercase letters?
+            if(word.toLowerCase().charAt(i)=='a'||word.toLowerCase().charAt(i)=='e'||word.toLowerCase().charAt(i)=='i'||word.toLowerCase().charAt(i)=='o'||word.toLowerCase().charAt(i)=='u'){
+                sum_vowel+=1;
+            }
+
+
+        
+        }
+        System.out.println("The number of vowels in the word "+word+" is "+sum_vowel);
+
+
+        System.out.print("Enter a positive interger to output that number of stars"); //how do i Uses a for loop to print a growing triangle pattern of stars, where the first line has 1 star, the second line has 2 stars, and so on, until the nth line has n stars.
+        int user_stars= scan.nextInt();
+        for(int i=0; i<user_stars; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
