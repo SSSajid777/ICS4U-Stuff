e8ee6cd74c13c20b3de12235c9be9cbe3f334e06
SSSajid <sajidihsanabdullah@gmail.com>
2026-03-03 14:28:08 -0500

    Update homework4.java

--- homework4.java
+++ homework4.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class homework4 {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in); 
+
+
+        System.out.println("Enter a positive integer"); //
+        int num=scan.nextInt();
+        while(num<0){
+            
+        }
     }
     
 }
