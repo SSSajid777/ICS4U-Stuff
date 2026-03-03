792ca85510b7741064db9b7976899f4e428e9cfb
SSSajid <sajidihsanabdullah@gmail.com>
2026-03-03 13:41:54 -0500

    Update forloops.java

--- forloops.java
+++ forloops.java
@@ -23,33 +23,34 @@ public class forloops {
          * delayclear();
          */
         /*
-        int total = 0;
-        System.out.println("Enter how many grades you want to average");
-        int get_amount = scan.nextInt();
-        for (int i = 1; i <= get_amount; i++) {
-            System.out.println("Enter grade " + i + ": ");         
-            double grade = scan.nextDouble();
-            total += grade;
-        }
-        System.out.println("The average of " + get_amount + " numbers is " + (total / get_amount));
-        */
+         * int total = 0;
+         * System.out.println("Enter how many grades you want to average");
+         * int get_amount = scan.nextInt();
+         * for (int i = 1; i <= get_amount; i++) {
+         * System.out.println("Enter grade " + i + ": ");
+         * double grade = scan.nextDouble();
+         * total += grade;
+         * }
+         * System.out.println("The average of " + get_amount + " numbers is " + (total /
+         * get_amount));
+         */
         /*
-        int counter=0;
-        System.out.println("Enter a word");
-        String word = scan.next();
-        for (int i=0; i <word.length(); i++){
-            if(word.charAt(i)=='a'||word.charAt(i)=='s'){
-                counter+=1;
+         * int counter=0;
+         * System.out.println("Enter a word");
+         * String word = scan.next();
+         * for (int i=0; i <word.length(); i++){
+         * if(word.charAt(i)=='a'||word.charAt(i)=='s'){
+         * counter+=1;
+         * }
+         * }
+         * System.out.println(word + " has the letter a and s"+ counter +" times");
+         */
+        for (int i = 1; i < 4; i++) {
+            System.out.print(i + " ");// prints the row number
+            for (int j = 1; j < 5; i++) {
+                System.out.print(j);// prints the column number
             }
-        }
-        System.out.println(word + " has the letter a and s"+ counter +" times");
-        */
-        for (int i=1; i <4; i++){
-            System.out.print(i+ " ");//prints the row number
-            for(int j=1; j<5; i++){
-                System.out.print(j);//prints the column number
-            }
-            System.out.println();//goes to the next row
+            System.out.println();// goes to the next row
 
         }
 
