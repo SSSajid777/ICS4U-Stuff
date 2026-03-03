35bdb7e4faa4ad16e0c3f8e10dafc29114e87f46
SSSajid <sajidihsanabdullah@gmail.com>
2026-03-03 13:41:34 -0500

    Update whileloops.java

--- whileloops.java
+++ whileloops.java
@@ -1,7 +1,21 @@
 import java.util.Scanner;
 
-public class whileloops{
+public class whileloops {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        int counter = 1;
+
+        while (counter < 11) {
+            System.out.print(counter + " ");
+            counter += 1; // or you could write counter++ or counter=counter + 1
+
+        }
+        int counter1 = 10;
+        while (counter1 > 0) {
+            System.out.print(counter1 + " ");
+            counter -= 1; // or you could write counter-- or counter=counter - 1
+
+        }
     }
 }
