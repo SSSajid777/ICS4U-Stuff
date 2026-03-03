2c77f9032eec0563226c7ee3777d74175d04b042
SSSajid <sajidihsanabdullah@gmail.com>
2026-03-03 14:19:23 -0500

    e

---
+++ homework4.java
@@ -0,0 +1,6 @@
+public class homework4 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- whileloops.java
+++ whileloops.java
@@ -35,14 +35,34 @@ public class whileloops {
          * break; //breaks the loops it is currently in
          * }
          * }
+         * 
+         * boolean decision = true;
+         * while (decision) {
+         * System.out.println("Input any number. Enter -1 to exit");
+         * int num1 = scan.nextInt();
+         * if (num1 == -1) {
+         * break; // breaks the loops it is currently in
+         * 
+         * }
+         * 
+         * }
          */
-        boolean decision = true;
-        while (decision) {
-            System.out.println("Input any number. Enter -1 to exit");
+        // this uses a loop inside of another loop
+        boolean decision1 = true;
+        while (decision1) {
+            System.out.println("Input a number");
             int num1 = scan.nextInt();
-            if (num1 == -1) {
-                break; // breaks the loops it is currently in
-
+            while (true) {
+                System.out.println("Would you like to run the program again, yes or no");
+                String answer = scan.next().toLowerCase();
+                if (answer.equals("yes")) {
+                    break;
+                } else if (answer.equals("no")) {
+                    decision1 = false;
+                    break;
+                } else {
+                    System.out.println("Only input yes or no");
+                }
             }
 
         }
