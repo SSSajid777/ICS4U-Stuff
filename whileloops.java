fc36e47aad57548f45c781cbb5268176ce8956b3
SSSajid <sajidihsanabdullah@gmail.com>
2026-03-03 13:51:07 -0500

    Update whileloops.java

--- whileloops.java
+++ whileloops.java
@@ -14,8 +14,16 @@ public class whileloops {
         int counter1 = 10;
         while (counter1 > 0) {
             System.out.print(counter1 + " ");
-            counter -= 1; // or you could write counter-- or counter=counter - 1
+            counter1 -= 1; // or you could write counter-- or counter=counter - 1
 
+        }
+
+        int num=1;
+        while(num !=1){
+            System.out.println("Input any number. Enter -1 to exit");
+            num = scan.nextInt();
+        
+            
         }
     }
 }
