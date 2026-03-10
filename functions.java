77ff4ddd3629dc7cf9e617dbd612a7864e915de5
SSSajid777 <sajidihsanabdullah@gmail.com>
2026-03-10 13:38:37 -0400

    Create functions.java

---
+++ functions.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class functions {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //this calls the method greeting
+        greeting();
+
+    }
+    //This is a method that does not pass a parameter or variable in the empty brackets
+    public static void greeting(){
+        System.out.println("Hello");
+
+    }
+}
