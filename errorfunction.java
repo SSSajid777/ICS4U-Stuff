31aaebffb7de8a190db267db6a620264fd638929
SSSajid <sajidihsanabdullah@gmail.com>
2026-03-11 13:35:33 -0400

    Update errorfunction.java

---
+++ errorfunction.java
@@ -0,0 +1,14 @@
+public class errorfunction{
+    public static void main(String[] args){
+
+    }
+    public static void menu(){
+        System.out.println("""
+            Menu
+            1.) item 1
+            2.) item 2
+            3) item 3
+        
+                """);
+    }
+}
