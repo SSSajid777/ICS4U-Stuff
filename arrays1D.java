115109b5361d8da4b2304899786f1ab8f7da030a
SSSajid <sajidihsanabdullah@gmail.com>
2026-03-23 13:41:45 -0400

    Create arrays1D.java

---
+++ arrays1D.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class arrays1D {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //the list nums starts at index 0 to index 2
+        //initializes a list with 3 items that are the same
+        int[] nums1={4,6,10};
+        System.out.println(nums1[0]); //outputs 4 from the list nums1
+
+    }
+}
