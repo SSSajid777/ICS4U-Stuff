731a8503a3e2d265d51d3bb1ddbc1e12abbb9b16
SSSajid777 <sajidihsanabdullah@gmail.com>
2026-03-24 14:08:54 -0400

    Update homework7.java

--- homework7.java
+++ homework7.java
@@ -9,6 +9,24 @@ public class homework7 {
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
 
-        <ArrayList>
+
+
+        double sum = 0;
+        System.out.println("Enter 10 numbers to get the sum and average");
+        int howmany = 10;
+        ArrayList<Double> numbers1 = new ArrayList<>();
+        for (int i = 0; i < howmany; i++) {
+            System.out.println("Input a number ");
+            // this variable is being used as a temporary variable for input
+            double temp = scan.nextDouble();
+            // this adds the temp value to the arraylist
+            numbers1.add(temp);
+            sum = sum + numbers1.get(i);
+        }
+        System.out.println("The sum of the numbers " + sum);
+        System.out.println("The average of the numbers " + sum/howmany);
+        
+
+        
     }
 }
