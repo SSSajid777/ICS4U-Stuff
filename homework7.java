7d2a11877c62a0775ee6a277a9a32ec9d1e7d831
SSSajid777 <sajidihsanabdullah@gmail.com>
2026-03-24 13:40:04 -0400

    Create homework7.java

---
+++ homework7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class homework7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        
+    }
+}
