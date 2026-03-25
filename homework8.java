6055a3fa330d35a854d2d4dcfdf706ae6b4873ba
SSSajid <sajidihsanabdullah@gmail.com>
2026-03-25 14:20:00 -0400

    Update homework8.java

--- homework8.java
+++ homework8.java
@@ -1,5 +1,6 @@
 public class homework8 {
     public static void main(String[] args) {
+        /* 
         int [][] nums1= new int[][] {{1,2,3}, {4,5,6}, {7,8,9}};
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
@@ -7,6 +8,18 @@ public class homework8 {
 
             }
             System.out.println();
+        }
+            */
+
+        int [][] nums2= new int[][] {{1,2,3}, {4,5,6}, {7,8,9}, {10, 11, 12}};
+        for(int i=0; i<4; i++){
+            int rowSum = 0;
+            for(int j=0; j<3; j++){
+                System.out.print(nums2[i][j]+" ");
+                rowSum+= nums2[i][j];
+
+            }
+            System.out.println("Row Sum " + rowSum);
         }
     }
 }
