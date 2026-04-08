c0addfaf4eb75e95cc4ff8a4aaee6f0c191da0f6
SSSajid777 <sajidihsanabdullah@gmail.com>
2026-04-08 13:23:46 -0400

    e

---
+++ avg1_homework9.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class avg1_homework9 extends avg2_homework9  {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ avg2_homework9.java
@@ -0,0 +1,5 @@
+public class avg2_homework9 {
+    public static void average1(int n1, int n2, int n3, int n4){
+        System.out.println("The average of the 4 numbers is " + ((n1+n2+n3+n4)/4));
+    }
+}
