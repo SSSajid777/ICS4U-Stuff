8a1b95ec135678c1fc1962e580da4f7b9a0776be
SSSajid777 <sajidihsanabdullah@gmail.com>
2026-04-08 14:25:29 -0400

    e

--- avg1_homework9.java
+++ avg1_homework9.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 public class avg1_homework9 extends avg2_homework9  {
     public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Please enter 4 numbers");
+        int num1, num2, num3, num4;
+        num1=scan.nextInt();
+        num2=scan.nextInt();
+        num3 =scan.nextInt();
+        num4 = scan.nextInt();
+        average1(num1, num2, num3, num4);
         
     }
 }
--- avg2_homework9.java
+++ avg2_homework9.java
@@ -1,5 +1,14 @@
-public class avg2_homework9 {
-    public static void average1(int n1, int n2, int n3, int n4){
-        System.out.println("The average of the 4 numbers is " + ((n1+n2+n3+n4)/4));
+    public class avg2_homework9 {
+        public static void average1(int n1, int n2, int n3, int n4){
+            int average1 = (n1+n2+n3+n4)/4;
+            System.out.println("The average of the 4 numbers is " + average1);
+            if (average1 >= 70) {
+                System.out.println(" High average");
+            } else if (average1>50 || average1<69){
+                System.out.println("Medium average");
+            }
+            else{
+                System.out.println("Low average");
+            }
+        }
     }
-}
