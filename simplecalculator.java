c7f2244a7d2c1b3da2a1848317e6fd152832482b
SSSajid777 <sajidihsanabdullah@gmail.com>
2026-04-14 14:28:24 -0400

    Update simplecalculator.java

--- simplecalculator.java
+++ simplecalculator.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class simpleCalculator extends JFrame implements ActionListener {
 
-    // the display at the top of the calculator
+    // display textfield - declared the same way as f1, f2, f3 in add2nums
     JTextField display;
 
     // number buttons
@@ -13,161 +13,85 @@ public class simpleCalculator extends JFrame implements ActionListener {
     // operation buttons
     JButton bAdd, bSub, bMul, bDiv, bEquals, bPlusMinus, bCE;
 
-    // keeps track of the first number entered and which operation was picked
-    double firstNum = 0;
+    // remembers the first number and the chosen operation
+    float firstNum = 0;
     String operator = "";
 
-    // when true, the next digit pressed will start a brand new number on the display
+    // true means the next digit starts a brand new number on the display
     boolean freshInput = true;
+
+    // layered pane from the MainMenu template
+    JLayeredPane layeredPane = new JLayeredPane();
 
     public simpleCalculator() {
 
         super("Simple Calculator");
-        setSize(400, 320);
+        setSize(480, 430);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // BorderLayout lets us put the display on top and buttons in the middle
-        // Source: https://docs.oracle.com/javase/tutorial/uiswing/layout/border.html
-        setLayout(new BorderLayout());
+        // same layered pane setup as MainMenu template
+        layeredPane.setPreferredSize(getSize());
+        setContentPane(layeredPane);
 
-        // set up the display textfield along the top
-        display = new JTextField("0");
+        // display textfield 
+        display = new JTextField(20);
         display.setEditable(false);
-        display.setHorizontalAlignment(JTextField.RIGHT);
-        display.setFont(new Font("Arial", Font.PLAIN, 22));
-        add(display, BorderLayout.NORTH);
+        display.setText("0");
+        display.setBounds(35, 50, 295, 42);
+        layeredPane.add(display, JLayeredPane.PALETTE_LAYER);
 
-        // a panel to hold all the buttons in a 5-row by 4-column grid
-        // GridLayout source: https://docs.oracle.com/javase/tutorial/uiswing/layout/grid.html
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(5, 4, 6, 6));
+        //NUMBER BUTTONS
+        b1 = new JButton("1");
+        b1.addActionListener(this);
+        b1.setBounds(35, 115, 80, 55);
+        layeredPane.add(b1, JLayeredPane.PALETTE_LAYER);
 
-        // creating all 17 buttons and hooking them up to actionPerformed
-        b1 = new JButton("1");       b1.addActionListener(this);
-        b2 = new JButton("2");       b2.addActionListener(this);
-        b3 = new JButton("3");       b3.addActionListener(this);
-        b4 = new JButton("4");       b4.addActionListener(this);
-        b5 = new JButton("5");       b5.addActionListener(this);
-        b6 = new JButton("6");       b6.addActionListener(this);
-        b7 = new JButton("7");       b7.addActionListener(this);
-        b8 = new JButton("8");       b8.addActionListener(this);
-        b9 = new JButton("9");       b9.addActionListener(this);
-        b0 = new JButton("0");       b0.addActionListener(this);
-        bAdd      = new JButton("+");   bAdd.addActionListener(this);
-        bSub      = new JButton("-");   bSub.addActionListener(this);
-        bMul      = new JButton("*");   bMul.addActionListener(this);
-        bDiv      = new JButton("/");   bDiv.addActionListener(this);
-        bEquals   = new JButton("=");   bEquals.addActionListener(this);
-        bPlusMinus = new JButton("+/-"); bPlusMinus.addActionListener(this);
-        bCE       = new JButton("CE");  bCE.addActionListener(this);
+        b2 = new JButton("2");
+        b2.addActionListener(this);
+        b2.setBounds(125, 115, 80, 55);
+        layeredPane.add(b2, JLayeredPane.PALETTE_LAYER);
 
-        // adding buttons in the same order as the example image
-        // row 1: 1, 2, 3, +
-        buttonPanel.add(b1);
-        buttonPanel.add(b2);
-        buttonPanel.add(b3);
-        buttonPanel.add(bAdd);
-        // row 2: 4, 5, 6, -
-        buttonPanel.add(b4);
-        buttonPanel.add(b5);
-        buttonPanel.add(b6);
-        buttonPanel.add(bSub);
-        // row 3: 7, 8, 9, *
-        buttonPanel.add(b7);
-        buttonPanel.add(b8);
-        buttonPanel.add(b9);
-        buttonPanel.add(bMul);
-        // row 4: 0, +/-, CE, /
-        buttonPanel.add(b0);
-        buttonPanel.add(bPlusMinus);
-        buttonPanel.add(bCE);
-        buttonPanel.add(bDiv);
-        // row 5: three empty spots then =, to match the layout in the example
-        buttonPanel.add(new JLabel());
-        buttonPanel.add(new JLabel());
-        buttonPanel.add(new JLabel());
-        buttonPanel.add(bEquals);
+        b3 = new JButton("3");
+        b3.addActionListener(this);
+        b3.setBounds(215, 115, 80, 55);
+        layeredPane.add(b3, JLayeredPane.PALETTE_LAYER);
 
-        add(buttonPanel, BorderLayout.CENTER);
-    }
+        b4 = new JButton("4");
+        b4.addActionListener(this);
+        b4.setBounds(35, 180, 80, 55);
+        layeredPane.add(b4, JLayeredPane.PALETTE_LAYER);
 
-    public void actionPerformed(ActionEvent e) {
+        b5 = new JButton("5");
+        b5.addActionListener(this);
+        b5.setBounds(125, 180, 80, 55);
+        layeredPane.add(b5, JLayeredPane.PALETTE_LAYER);
 
-        String action = e.getActionCommand();
 
-        // if a number was pressed, either start fresh or append to what's already there
-        if (action.equals("0") || action.equals("1") || action.equals("2") ||
-            action.equals("3") || action.equals("4") || action.equals("5") ||
-            action.equals("6") || action.equals("7") || action.equals("8") ||
-            action.equals("9")) {
+        b6 = new JButton("6");
+        b5.addActionListener(this);
+        b5.setBounds(125, 180, 80, 55);
+        layeredPane.add(b5, JLayeredPane.PALETTE_LAYER);
 
-            if (freshInput) {
-                display.setText(action);
-                freshInput = false;
-            } else {
-                display.setText(display.getText() + action);
-            }
-        }
 
-        // if an operator was pressed, save the current number and wait for the second one
-        if (action.equals("+") || action.equals("-") || action.equals("*") || action.equals("/")) {
-            firstNum = Double.parseDouble(display.getText());
-            operator = action;
-            freshInput = true;
-        }
 
-        // equals button: do the math and show the result
-        if (action.equals("=")) {
-            double secondNum = Double.parseDouble(display.getText());
-            double result = 0;
+        b7 = new JButton("7");
+        b5.addActionListener(this);
+        b5.setBounds(125, 180, 80, 55);
+        layeredPane.add(b5, JLayeredPane.PALETTE_LAYER);
 
-            if (operator.equals("+")) result = firstNum + secondNum;
-            if (operator.equals("-")) result = firstNum - secondNum;
-            if (operator.equals("*")) result = firstNum * secondNum;
-            if (operator.equals("/")) {
-                // can't divide by zero
-                if (secondNum == 0) {
-                    display.setText("Error");
-                    freshInput = true;
-                    return;
-                }
-                result = firstNum / secondNum;
-            }
 
-            // if the result is a whole number, drop the ".0" so it looks clean
-            if (result % 1 == 0) {
-                display.setText(String.valueOf((int) result));
-            } else {
-                display.setText(String.valueOf(result));
-            }
 
-            freshInput = true;
-        }
+        b8 = new JButton("8");
+        b5.addActionListener(this);
+        b5.setBounds(125, 180, 80, 55);
+        layeredPane.add(b5, JLayeredPane.PALETTE_LAYER);
 
-        // flip the sign of whatever is on the display
-        if (action.equals("+/-")) {
-            if (!display.getText().equals("0") && !display.getText().equals("Error")) {
-                double val = Double.parseDouble(display.getText());
-                val = val * -1;
-                if (val % 1 == 0) {
-                    display.setText(String.valueOf((int) val));
-                } else {
-                    display.setText(String.valueOf(val));
-                }
-            }
-        }
 
-        // CE wipes everything and resets the calculator to its starting state
-        if (action.equals("CE")) {
-            display.setText("0");
-            firstNum = 0;
-            operator = "";
-            freshInput = true;
-        }
-    }
 
-    public static void main(String args[]) {
-        simpleCalculator x = new simpleCalculator();
-        x.setVisible(true);
-    }
-}
+        b5 = new JButton("5");
+        b5.addActionListener(this);
+        b5.setBounds(125, 180, 80, 55);
+        layeredPane.add(b5, JLayeredPane.PALETTE_LAYER);
+
+        
+
