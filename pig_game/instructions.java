38b53b2635ed2b63094c477629fdff51d0651ac0
SSSajid777 <sajidihsanabdullah@gmail.com>
2026-04-27 11:20:34 -0400

    Update instructions.java

--- pig_game/instructions.java
+++ pig_game/instructions.java
@@ -6,30 +6,8 @@ public class instructions extends JFrame implements ActionListener {
 
     // create buttons
     public instructions() {
-        JTextArea f1;
 
-        String intro = """
-
-Instructions:
-
-Each turn, a player repeatedly rolls two dice until a 1 is rolled or the player decides to hold.
-
-If a single 1 is rolled, the player scores nothing and the turn ends.
-
-If two 1s are rolled, the player's entire score is lost and the turn ends.
-
-If the player rolls any other number, it is added to their turn total and the turn continues.
-
-If a double is rolled, the total is added but the player must roll again.
-
-If a player chooses to hold, their turn total is added to their score and it becomes the next player's turn.
-
-The first player to 100 or more points wins.
-                """;
-
-        f1 = new JTextArea();
-
-        setSize(975, 500);
+        setSize(1300, 700);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -37,23 +15,24 @@ The first player to 100 or more points wins.
         layeredPane.setPreferredSize(getSize());
         setContentPane(layeredPane);
 
-        f1.setBounds(0, 0, 700, 400);
-        layeredPane.add(f1, JLayeredPane.PALETTE_LAYER);
-        f1.setText(String.valueOf(intro));
+        ImageIcon image1 = new ImageIcon("rules.png");
+        JLabel bg = new JLabel(image1);
+        bg.setBounds(0, 0, image1.getIconWidth(), image1.getIconHeight());
+        layeredPane.add(bg, Integer.valueOf(0));
 
         // creates the buttons and sets them up to be used in actionPerformed
         JButton b1 = new JButton("Exit");
-        b1.setBounds(65, 400, 70, 35);
+        b1.setBounds(150, 610, 200, 50);
         b1.addActionListener(this);
         layeredPane.add(b1, JLayeredPane.PALETTE_LAYER);
 
         JButton b2 = new JButton("Menu");
-        b2.setBounds(170, 400, 70, 35);
+        b2.setBounds(550, 610, 200, 50);
         b2.addActionListener(this);
         layeredPane.add(b2, JLayeredPane.PALETTE_LAYER);
 
         JButton b3 = new JButton("Play");
-        b3.setBounds(275, 400, 70, 35);
+        b3.setBounds(950, 610, 200, 50);
         b3.addActionListener(this);
         layeredPane.add(b3, JLayeredPane.PALETTE_LAYER);
     }
